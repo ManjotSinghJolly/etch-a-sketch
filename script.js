c7fea7b8e016247e3744e948ff54a109cd847129
Manjot Singh Jolly <manjotsinghjolly@gmail.com>
2023-12-26 14:23:55 +0530

    Add Rainbow Button functionality

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ boxes.forEach(function (box) {
 //Erase functionality
 const eraser = document.getElementById("erase");
 eraser.addEventListener("click", function () {
-  console.log("I think the erase button was really clicked this time.");
+  // console.log("I think the erase button was really clicked this time.");
   boxes.forEach(function (box) {
     box.addEventListener("mouseover", function () {
       box.style.backgroundColor = "white";
@@ -83,7 +83,77 @@ const colorPicker = document.getElementById("colour-picker");
 //Click function for the colour picker
 colorPicker.addEventListener("change", function (colorValue) {
   color = colorValue.target.value;
-  console.log("The value of the new color is: " + color);
+  // console.log("The value of the new color is: " + color);
 });
 
+function generateRandomColor() {
+  let alphabets = ["a", "b", "c", "d", "e", "f"];
+  let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // let randomAlphabet = Math.floor(Math.random() * 10);
+  // console.log(randomAlphabet);
+  // let randomNumber = Math.floor(Math.random() * 6);
+  // console.log("The random alphabet is: " + alphabets[randomAlphabet]);
+  // console.log("The random number is: " + numbers[randomNumber]);
+  hexCodeList = [];
+  let randomAlphabet;
+  let randomNumber;
+  // Loop for the alphabets
+  let randomCount = Math.floor(Math.random() * 6) + 1;
+
+  for (let j = 1; j <= randomCount; j++) {
+    randomAlphabet = Math.floor(Math.random() * 6);
+    hexCodeList.push(alphabets[randomAlphabet]);
+
+    // console.log("Content at iteration " + j + ": " + hexCodeList);
+  }
+  let listLength = hexCodeList.length;
+  // console.log("Length: " + listLength);
+  let lengthLeft = 6 - listLength;
+
+  // Loop for numbers
+  if (lengthLeft < 6) {
+    for (let k = 1; k <= lengthLeft; k++) {
+      randomNumber = Math.floor(Math.random() * 10);
+      hexCodeList.push(numbers[randomNumber]);
+      // console.log("The content at iteration " + k + " is: " + hexCodeList);
+    }
+  }
+
+  // console.log("The hex code list is: " + hexCodeList);
+
+  //Printing the hexCodeList in random order
+  // Printing the hexCodeList in random order
+  randomHexList = [];
+  let remainingIndices = [0, 1, 2, 3, 4, 5];
+
+  for (let l = 1; l <= 6; l++) {
+    const randomIndex = Math.floor(Math.random() * remainingIndices.length);
+    const selectedIndex = remainingIndices.splice(randomIndex, 1)[0];
+    randomHexList.push(hexCodeList[selectedIndex]);
+  }
+
+  // console.log("The final content is: " + randomHexList);
+
+  // Join the list into a string
+  const hexCode = randomHexList.join("");
+
+  // Use the hexCode as a color option
+  const colorOption = "#" + hexCode;
+
+  return colorOption;
+}
+
+// console.log(colorOption);
+
+// console.log("Okay, The color option is: " + resultColor);
+
 //Rainbow mode
+let rainbowButton = document.getElementById("rainbow");
+rainbowButton.addEventListener("click", function () {
+  boxes.forEach(function (box) {
+    box.addEventListener("mouseover", function () {
+      let resultColor = generateRandomColor();
+      box.style.backgroundColor = resultColor;
+    });
+  });
+});
